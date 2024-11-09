6634502ebd54664a2b86a8aecfa902e6068693b1
jbangel7 <jbangel@unal.edu.co>
2024-11-08 20:17:16 -0500

    First try

--- homework/word_count.py
+++ homework/word_count.py
@@ -3,8 +3,9 @@
 # pylint: disable=broad-exception-raised
 
 import fileinput
-import glob
+import glob # libreria para leer directorios y saber archivos tienen (Jomba)
 import os.path
+import string # libreria para borrar puntuaciones
 from itertools import groupby
 
 
@@ -25,6 +26,12 @@ from itertools import groupby
 #
 def load_input(input_directory):
     """Funcion load_input"""
+    files = glob.glob(f"{input_directory}/*")
+    sequence = []
+    with fileinput.input(files=files) as f:
+        for line in f:
+            sequence.append((fileinput.filename(), line))
+    return sequence    
 
 
 #
@@ -34,6 +41,22 @@ def load_input(input_directory):
 #
 def line_preprocessing(sequence):
     """Line Preprocessing"""
+
+    sequence = [
+        (key, value.translate(str.maketrans("", "", string.punctuation))) # maketrans elimina toda la puntuacion
+        for key, value in sequence
+    ]    
+
+    sequence = [
+        (key, value.lower())  # convertir todo a minusculas
+        for key, value in sequence
+    ]
+
+    sequence = [
+        (key, value.strip())  #strip elimina los espacios vacios
+        for key, value in sequence
+    ]
+    return sequence
 
 
 #
@@ -50,6 +73,8 @@ def line_preprocessing(sequence):
 #
 def mapper(sequence):
     """Mapper"""
+    return [(word, 1) for _, text_line in sequence for word in text_line.split()]
+
 
 
 #
@@ -65,6 +90,7 @@ def mapper(sequence):
 #
 def shuffle_and_sort(sequence):
     """Shuffle and Sort"""
+    return sorted(sequence, key=lambda x: x[0])
 
 
 #
@@ -75,6 +101,13 @@ def shuffle_and_sort(sequence):
 #
 def reducer(sequence):
     """Reducer"""
+    diccionario = {}
+    for key, value in sequence:
+        if key in diccionario:
+            diccionario[key] += value
+        else:
+            diccionario[key] = value
+    return list(diccionario.items())
 
 
 #
@@ -83,6 +116,11 @@ def reducer(sequence):
 #
 def create_ouptput_directory(output_directory):
     """Create Output Directory"""
+    if os.path.exists(output_directory):
+        for file in glob.glob(f"{output_directory}/*"):
+            os.remove(file)
+        os.rmdir(output_directory)
+    os.makedirs(output_directory)
 
 
 #
@@ -95,6 +133,9 @@ def create_ouptput_directory(output_directory):
 #
 def save_output(output_directory, sequence):
     """Save Output"""
+    with open(f"{output_directory}/part-00000", "w", encoding="utf-8") as f:
+        for key, value in sequence:
+            f.write(f"{key}\t{value}\n")
 
 
 #
@@ -103,6 +144,8 @@ def save_output(output_directory, sequence):
 #
 def create_marker(output_directory):
     """Create Marker"""
+    with open(f"{output_directory}/_SUCCESS", "w", encoding="utf-8") as f:
+        f.write("")
 
 
 #
@@ -110,10 +153,19 @@ def create_marker(output_directory):
 #
 def run_job(input_directory, output_directory):
     """Job"""
-
+    sequence = load_input(input_directory)
+    sequence = line_preprocessing(sequence)
+    sequence = mapper(sequence)
+    sequence = shuffle_and_sort(sequence)
+    sequence = reducer(sequence)
+    
+    create_ouptput_directory(output_directory)
+    save_output(output_directory, sequence)
+    create_marker(output_directory)
+    
 
 if __name__ == "__main__":
     run_job(
-        "input",
-        "output",
+        "files/input",
+        "files/output",
     )
